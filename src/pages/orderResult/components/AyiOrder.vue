f4acbd2665688349f0c687025f323caeab0b76ea
huyajie <hu_ya_jie@163.com>
2019-04-26 19:11:38 +0800

    图片修改

--- src/pages/orderResult/components/AyiOrder.vue
+++ src/pages/orderResult/components/AyiOrder.vue
@@ -2,7 +2,7 @@
   <div class="ayi-result">
     <template v-if="state =='1'">
       <div class="state-box">
-        <img class="img" src="../../../assets/images/yuyue/yycg.png" alt>
+        <img class="img" src="../../../assets/images/state/right.png" alt>
       </div>
       <div class="state-text">预约成功</div>
       <div class="desc">工作人员会尽快联系您！</div>
@@ -10,16 +10,14 @@
 
     <template v-if="state =='2'">
       <div class="state-box">
-        <img class="img" src="../../../assets/images/yuyue/yysb.png" alt>
+        <img class="img" src="../../../assets/images/state/err.png" alt>
       </div>
       <div class="state-text">预约失败</div>
       <div class="desc">您可致电 17346578021 预约阿姨！</div>
     </template>
   </div>
 </template>
-  </div>
-
-</template>
+  
 
 <script>
 export default {
